852b5e9d59a8187ed4ccee720b5d3983159dfcec
vlavrent <73958916+vlavrent@users.noreply.github.com>
2020-11-30 22:15:38 +0200

    Update Skyline.scala

--- src/main/scala/Skyline.scala
+++ src/main/scala/Skyline.scala
@@ -9,10 +9,71 @@ import org.apache.spark
 
 
  object bigdata {
+  /** Calculates dominance score for all points in the given dataframe taking into considerations the dominated
+   *  points in the given dataframe
+   *
+   *  @param df given dataframe
+   *  @param sparkSession the spark session to call sql
+   *  @return a dataframe of points with their dominance score
+   */
+  def calculate_dominance_score(df:DataFrame,joinDF:DataFrame, sparkSession: SparkSession): DataFrame ={
 
+
+    val scored_df_without_zero = joinDF.groupBy(col("lid").alias("id"))
+      .agg(size(collect_list("rid")).alias("score"))
+
+    val scored_df = df.select("id").join(scored_df_without_zero, Seq("id"), "left_outer").na.fill(0, Seq("score"))
+
+    scored_df
+  }
+
+  def calculate_skyline_score(df: DataFrame,joinDF:DataFrame, sparkSession: SparkSession): DataFrame ={
+
+
+    val scored_df = calculate_dominance_score(df,joinDF, sparkSession)
+
+    val zero = scored_df.select("id","score").where("score == 0")
+    val sky = zero.select("id").except(joinDF.select("rid").distinct())
+    val skyline = joinDF.select("lid").except(joinDF.select("rid")).withColumnRenamed("lid", "id")
+      .union(sky.select("id"))
+    val skyline_scores = skyline.join(scored_df, "id")
+
+    skyline_scores
+
+  }
+
+  def task1_3(k:Int,df:DataFrame,joinDF:DataFrame, sparkSession: SparkSession): Unit = {
+
+
+
+    val skyline_score = calculate_skyline_score(df,joinDF, sparkSession)
+
+    val skyline = skyline_score.select("id")
+    print(sparkSession.time(skyline.sort(col("id").desc).take(k).mkString("(", ", ", ")")))
+    print(sparkSession.time(skyline_score.sort(col("score").desc).take(k).mkString("(", ", ", ")")))
+
+  }
+
+  def task2(k:Int, df:DataFrame, joinDF:DataFrame, sparkSession: SparkSession): Unit ={
+
+
+
+    //    val x_mean = df.select(avg("x")).first().getDouble(0)
+    //    val y_mean = df.select(avg("y")).first().getDouble(0)
+    //
+    //    val quartile_1 = df.filter("x <" + x_mean + " AND  y <" + y_mean) //down left
+    //    val quartile_2 = df.filter("x <" + x_mean + " AND  y >" + y_mean) //up left
+    //    val quartile_3 = df.filter("x >" + x_mean + " AND  y <" + y_mean) //down right
+    //    val quartile_4 = df.filter("x >" + x_mean + " AND  y >" + y_mean) //up right
+    //
+    //    val dominated_quartile_count = quartile_4.count()
+
+    val scoresDf = calculate_dominance_score(df,joinDF, sparkSession)
+
+    print(sparkSession.time(scoresDf.sort(col("score").desc).take(k).mkString("(", ", ", ")")))
+  }
 
   def main(args: Array[String]): Unit = {
-
     Logger.getLogger("org").setLevel(Level.WARN)
     Logger.getLogger("akka").setLevel(Level.WARN)
 
@@ -22,43 +83,22 @@ import org.apache.spark
       .appName("Skyline")
       .getOrCreate()
 
+    val k = 10
+    val dataset_path = "src/main/Resource/Normal.csv"
 
-
-    val df = sparkSession.read.option("header", "true").csv("src/main/Resource/Normal.csv")
+    val df = sparkSession.read.option("header", "true").csv(dataset_path)
       .select(col("0").alias("x"), col("1").alias("y"), col("id"))
 
+    df.createOrReplaceTempView("df")
+    //SQL JOIN
+    val joinDF = sparkSession.sql("select l.id AS lid, r.id AS rid " +
+      "from df l, df r " +
+      "where l.x < r.x and l.y < r.y ")
 
-      df.createOrReplaceTempView("df")
-
-      //SQL JOIN
-      val joinDF = sparkSession.sql("select l.id AS lid, r.id AS rid " +
-        "from df l, df r " +
-        "where l.x < r.x and l.y < r.y ")
-      println(joinDF.show())
-
-      val scored_df_without_zero = joinDF.groupBy(col("lid").alias("id"))
-        .agg(size(collect_list("rid")).alias("score"))
-
-
-      val scored_df = df.select("id").join(scored_df_without_zero, Seq("id"), "left_outer").na.fill(0, Seq("score"))
-
-
-      //Skyline
-      val search_null = df.select("id").join(scored_df_without_zero, Seq("id"), "left_outer")
-      val keep_null = search_null.select(col("id")).where(col("score").isNull)
-      val not_dom_null = keep_null.select("id").except(joinDF.select("rid").distinct())
-      val skyline = joinDF.select("lid").except(joinDF.select("rid")).withColumnRenamed("lid", "id")
-        .union(not_dom_null.select("id"))
-      val skyline_scores = skyline.join(scored_df, "id")
-    //println(skyline.sort(col("score").desc).show(10))
-    //println(scoresDF.sort(col("score").desc).show(10))
-
-
-    //scoresDF.write.csv("D://Vicky//Data and Web Science//Big Data//Dominate.csv")
-    //skyline.write.csv("D://Vicky//Data and Web Science//Big Data//Skyline.csv")
+    task1_3(k.toInt,df,joinDF,sparkSession)
+    task2(k.toInt, df, joinDF, sparkSession)
 
 
 
-  }
-
-}
+ }
+ }
