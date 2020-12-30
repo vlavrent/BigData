0cfc3e1b5a12b06325da84d7ce76dd76b53d1e1b
cogian <kgiantsios@atypon.com>
2020-12-30 14:16:44 +0200

    change some parameters for spark

--- src/main/scala/dominanceScore_3d.scala
+++ src/main/scala/dominanceScore_3d.scala
@@ -15,10 +15,12 @@ object dominanceScore_3d {
 		Logger.getLogger("org").setLevel(Level.WARN)
 		Logger.getLogger("akka").setLevel(Level.WARN)
 
-		val conf = new SparkConf().setMaster("local[*]").setAppName("Skyline").set("spark.executor.memory", "12g")
+		val conf = new SparkConf()
+			.setMaster("local[*]")
+			.setAppName("DominanceScore2d_top" + k + "_" + dataset_path + "_" + x_axis_size + "_" + y_axis_size + "_" + z_axis_size)
+			.set("spark.executor.memory", "12g")
 		val sparkSession = SparkSession.builder
 			.config(conf = conf)
-			.appName("Skyline")
 			.getOrCreate()
 
 		//correlated/correlated50000.csv
--- src/main/scala/dominanceScore_4d.scala
+++ src/main/scala/dominanceScore_4d.scala
@@ -16,10 +16,12 @@ object dominanceScore_4d {
 		Logger.getLogger("org").setLevel(Level.WARN)
 		Logger.getLogger("akka").setLevel(Level.WARN)
 
-		val conf = new SparkConf().setMaster("local[*]").setAppName("Skyline").set("spark.executor.memory", "12g")
+		val conf = new SparkConf()
+			.setMaster("local[*]")
+			.setAppName("DominanceScore2d_top" + k + "_" + dataset_path + "_" + x_axis_size + "_" + y_axis_size + "_" + z_axis_size + "_" + t_axis_size)
+			.set("spark.executor.memory", "12g")
 		val sparkSession = SparkSession.builder
 			.config(conf = conf)
-			.appName("Skyline")
 			.getOrCreate()
 
 		//correlated/correlated50000.csv
