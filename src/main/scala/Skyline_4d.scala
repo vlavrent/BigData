7182787497125892dc88f69fda0b837f43bd0504
vlavrent <73958916+vlavrent@users.noreply.github.com>
2021-01-04 22:59:25 +0200

    4d Skyline with rdd

--- src/main/scala/Skyline_4d.scala
+++ src/main/scala/Skyline_4d.scala
@@ -1,87 +1,107 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.sql.functions.{collect_list, size}
-import org.apache.spark.sql.functions.{ col, max,min}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.{Row, SparkSession}
 
 
 import scala.collection.mutable.ListBuffer
 
 object Skyline_4d {
-  def Find_mins(total: DataFrame,rank_x: DataFrame,sparkSession: SparkSession) = {
-    import sparkSession.implicits._
-
-    val y_value = total.select(total("y").cast("double")).map(_.getDouble(0)).collect.toList
-    val id = total.select(total("id").cast("Long")).map(_.getLong(0)).collect.toList
-    var min_y = rank_x.select(max("y").cast("double")).first().getDouble(0)
-    var tmin = min_y+1
-    var skyline = new ListBuffer[Long]()
-
-    for((y,idy)<-y_value zip id) if(y<tmin){tmin = y;skyline += idy}
-
-    val values_sky = skyline.toList
-
-    values_sky.toDF()
-
-  }
-  def task1(dataset_path :String): Unit = {
-
+  def SaveCSV(saveSkyline:RDD[(Double,Double,Double,Double,Double)]){
     Logger.getLogger("org").setLevel(Level.WARN)
     Logger.getLogger("akka").setLevel(Level.WARN)
 
-    val conf = new SparkConf().setMaster("local[*]").setAppName("Skyline")
+
+    val conf = new SparkConf().setMaster("local[1]").setAppName("Skyline")
     val sparkSession = SparkSession.builder
       .config(conf = conf)
       .appName("Skyline")
       .getOrCreate()
     import sparkSession.implicits._
-    //numPartitions
 
-
-
-    val df = sparkSession.read.option("header", "true").csv(dataset_path)
-      .select(col("0").alias("x"), col("1").alias("y"),col("2").alias("z"), col("3").alias("w"), col("id"))
-
-
-    val sort_x = df.orderBy("x")
-    val miny = sort_x.select(min("y")).first().getString(0)
-    val minz = sort_x.select(min("z")).first().getString(0)
-    val minw = sort_x.select(min("w")).first().getString(0)
-
-    val RanksXY = sort_x.select("x","y","id")
-    val RanksXZ = sort_x.select("x","z","id")
-    val RanksXW = sort_x.select("x","w","id")
-
-    val minxy = RanksXY.select(RanksXY("x").cast("String")).where("y=="+miny).first().getString(0)
-    val minxz = RanksXZ.select(RanksXZ("x").cast("String")).where("z=="+minz).first().getString(0)
-    val minxw = RanksXW.select(RanksXW("x").cast("String")).where("w=="+minw).first().getString(0)
-
-    val FilterXY = RanksXY.filter("x<="+minxy)
-    val FilterXZ = RanksXZ.filter("x<="+minxz)
-    val FilterXW = RanksXW.filter("x<="+minxw)
-    //print(filtered_ranks.show())
-    //print(rank_x.show())
-
-    val SkyXY = Find_mins(FilterXY,RanksXY,sparkSession)
-    val SkyXZ = Find_mins(FilterXZ,RanksXZ,sparkSession)
-    val SkyXW = Find_mins(FilterXW,RanksXW,sparkSession)
-    val skyline = SkyXY.union(SkyXZ).union(SkyXW).groupBy(col("value"))
-      .agg(size(collect_list("value")).alias("id"))
-    print(skyline.show())
-    //skyline.write.csv("skyline.csv")
-
+    val save = saveSkyline.coalesce(1).toDF("0","1","2","3","id")
+    print(saveSkyline.toDF().show())
+    save.write.option("header", "true").csv("skyline4d.csv")
 
 
   }
+
+  def task1(dataset_path:String): RDD[(Double,Double,Double,Double,Double)] ={
+    Logger.getLogger("org").setLevel(Level.WARN)
+    Logger.getLogger("akka").setLevel(Level.WARN)
+
+
+    val conf = new SparkConf().setMaster("local[8]").setAppName("Skyline")
+    val sc = new SparkContext(conf)
+
+    //Read csv and remove headers
+    val rddFromFile = sc.textFile(dataset_path,4)
+    val header = rddFromFile.first()
+    val rdd1 = rddFromFile.filter(row => row != header).map(f=>{f.split(",")})
+
+    //Finding Local Skylines
+    val rdd = rdd1.map(n => {(n.map(_.toDouble))}).map(n=>Tuple5(n(0),n(1),n(2),n(3),n(4)))
+      .sortBy(_._1,true)
+      .mapPartitions( iterator => {
+
+        var miny = 2000.0
+        var minz = 2000.0
+        var minw = 2000.0
+        var sky : List[(Double,Double,Double,Double,Double)] = List()
+        while(iterator.hasNext) {
+          var it = iterator.next()
+          if(it._2<miny && it._3<minz && it._4<minw){miny=it._2;minz=it._3;minw=it._4;sky =sky:+ (it._1,it._2,it._3,it._4,it._5)}
+          else if(it._2<miny && it._3>minz && it._4>minw){miny=it._2;sky =sky:+ (it._1,it._2,it._3,it._4,it._5)}
+          else if(it._2>miny && it._3<minz && it._4>minw){minz=it._3;sky =sky:+ (it._1,it._2,it._3,it._4,it._5)}
+          else if(it._2>miny && it._3>minz && it._4<minw){minw=it._4;sky =sky:+ (it._1,it._2,it._3,it._4,it._5)}
+          else if(it._2<miny && it._3<minz && it._4>minw){miny=it._2;minz=it._3;sky =sky:+ (it._1,it._2,it._3,it._4,it._5)}
+          else if(it._2<miny && it._3>minz && it._4<minw){miny=it._2;minw=it._4;sky =sky:+ (it._1,it._2,it._3,it._4,it._5)}
+          else if(it._2>miny && it._3<minz && it._4<minw){minz=it._3;minw=it._4;sky =sky:+ (it._1,it._2,it._3,it._4,it._5)}
+        }
+        (sky.toIterator)
+
+      }).collect()
+
+
+    //Finding Global Skyline
+    var miny = 2000.0
+    var minz = 2000.0
+    var minw = 2000.0
+    var skyline : List[(Double,Double,Double,Double,Double)] = List()
+
+
+    rdd.sortBy(_._1).map(x=>{
+      if(x._2<miny && x._3<minz && x._4<minw){miny=x._2;minz=x._3;minw=x._4;skyline = skyline:+ (x._1,x._2,x._3,x._4,x._5);}
+      else if(x._2<miny && x._3>minz && x._4>minw){miny=x._2;skyline =skyline:+ (x._1,x._2,x._3,x._4,x._5)}
+      else if(x._2>miny && x._3<minz && x._4>minw){minz=x._3;skyline =skyline:+ (x._1,x._2,x._3,x._4,x._5)}
+      else if(x._2>miny && x._3>minz && x._4<minw){minw=x._4;skyline =skyline:+ (x._1,x._2,x._3,x._4,x._5)}
+      else if(x._2<miny && x._3<minz && x._4>minw){miny=x._2;minz=x._3;skyline =skyline:+ (x._1,x._2,x._3,x._4,x._5)}
+      else if(x._2<miny && x._3>minz && x._4<minw){miny=x._2;minw=x._4;skyline =skyline:+ (x._1,x._2,x._3,x._4,x._5)}
+      else if(x._2>miny && x._3<minz && x._4<minw){minz=x._3;minw=x._4;skyline =skyline:+ (x._1,x._2,x._3,x._4,x._5)}
+      ; (skyline)})
+
+    println(skyline)
+
+    //Convert List to RDD and return
+    val saveSkyline = sc.parallelize(skyline)
+
+    saveSkyline
+
+
+  }
+
   def main(args: Array[String]): Unit = {
 
     val dataset_path = args(0)
 
-
     val t1 = System.nanoTime
-    task1(dataset_path )
+    val saveSkyline = task1(dataset_path)
     val duration = (System.nanoTime - t1)
     print(duration)
+
+    //Save Skyline
+    SaveCSV(saveSkyline)
+
 
 
   }
