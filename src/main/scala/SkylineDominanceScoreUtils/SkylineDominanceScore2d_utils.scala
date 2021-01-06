20e5d01bc1b59678861ecc6270a2df62aea92799
cogian <kgiantsios@atypon.com>
2021-01-06 12:47:49 +0200

    intialize task 3

---
+++ src/main/scala/SkylineDominanceScoreUtils/SkylineDominanceScore2d_utils.scala
@@ -0,0 +1,111 @@
+package SkylineDominanceScoreUtils
+
+import DominanceScoreUtils.dominanceScore_2d_utils.get_cell_borders
+import org.apache.spark.sql.DataFrame
+
+import scala.collection.mutable.ListBuffer
+
+object SkylineDominanceScore2d_utils {
+
+	/** Creates the grid cells to check with pruning
+	 *
+	 *  @param df given dataframe size of x axis
+	 *  @param x_max max point of x axis
+	 *  @param y_max max point of y axis
+	 *  @param x_axis list with x axis lines
+	 *  @param y_axis list with x axis lines
+	 *  @param k top k dominant points
+	 *  @return a list with cells that must be checked
+	 */
+	def create_grid_cells_to_check_2d(
+																		 df: DataFrame,
+																		 x_max:Double,
+																		 y_max:Double,
+																		 x_axis:List[Double],
+																		 y_axis:List[Double],
+																		 skyline: List[(Double, Double, Int)],
+																		 k:Int): ListBuffer[((Int, Int), (Int, Double, Double, Double, Double), (Int, Int))] ={
+
+		val x_axis_size:Int = x_axis.size
+		val y_axis_size:Int = y_axis.size
+		var grid_cells_with_counts = new ListBuffer[((Int, Int), (Int, Double, Double, Double, Double))]
+		for(i <- 0 to x_axis_size){
+			for(j <- 0 to y_axis_size){
+				val grid_cell = (i, j)
+				val borders = get_cell_borders(
+					x_max,
+					y_max,
+					grid_cell,
+					x_axis,
+					y_axis)
+
+				val x_line_left = borders(0)
+				val x_line_right = borders(1)
+				val y_line_up = borders(2)
+				val y_line_down = borders(3)
+
+				val number_of_points_in_cell = df.filter("x <= " + x_line_right + " AND y <= " + y_line_up +
+					" AND " + " x > " + x_line_left + " AND  y > " + y_line_down).count().toInt
+
+				//
+				grid_cells_with_counts.append(
+					(grid_cell, (number_of_points_in_cell, x_line_left, x_line_right, y_line_up, y_line_down)))
+
+			}
+		}
+
+		val grid_cells_with_counts_map = grid_cells_with_counts.toMap
+
+		var candidate_grid_cells = new ListBuffer[((Int, Int), (Int, Double, Double, Double, Double))]
+		for(grid_cell <- grid_cells_with_counts_map){
+			var number_of_dominating_points = 0
+
+			for(cell <- grid_cells_with_counts){
+				if (cell._1._1 < grid_cell._1._1 && cell._1._2 < grid_cell._1._2)
+					number_of_dominating_points += cell._2._1
+			}
+
+			if(number_of_dominating_points < k && grid_cell._2._1 > 0)
+				candidate_grid_cells.append(grid_cell)
+		}
+
+		var candidate_grid_cells_with_bound_scores =  new ListBuffer[((Int, Int), (Int, Double, Double, Double, Double), (Int, Int))]
+		for(grid_cell <- candidate_grid_cells){
+
+			var partially_dominated_points_count = 0
+			var fully_dominated_points = 0
+
+			for(cell <- grid_cells_with_counts) {
+				if (cell._1._1 == grid_cell._1._1 || cell._1._2 == grid_cell._1._2) {
+					if (cell._1._1 >= grid_cell._1._1 && cell._1._2 >= grid_cell._1._2)
+						partially_dominated_points_count += cell._2._1
+				}
+				else if(cell._1._1 > grid_cell._1._1 && cell._1._2 > grid_cell._1._2) {
+					fully_dominated_points += cell._2._1
+				}
+			}
+
+			partially_dominated_points_count -= grid_cell._2._1
+			candidate_grid_cells_with_bound_scores.append(
+				(grid_cell._1,
+					grid_cell._2,
+					(fully_dominated_points,
+						fully_dominated_points + partially_dominated_points_count)))
+		}
+
+		var lower_bound_score = -1
+		for( grid_cell <- candidate_grid_cells_with_bound_scores)
+			if(grid_cell._2._1 >= k && grid_cell._3._1 > lower_bound_score)
+				lower_bound_score = grid_cell._3._1
+
+
+		var prunned_candidate_grid_cells = new ListBuffer[((Int, Int), (Int, Double, Double, Double, Double), (Int, Int))]
+		for( grid_cell <- candidate_grid_cells_with_bound_scores)
+			if(grid_cell._3._2 >= lower_bound_score)
+				prunned_candidate_grid_cells.append(grid_cell)
+
+
+		prunned_candidate_grid_cells
+	}
+
+}
---
+++ src/main/scala/Skyline_dominanceScore_2d.scala
@@ -0,0 +1,103 @@
+import DominanceScoreUtils.dominanceScore_2d_utils.{calculate_dominance_score_2d, create_grid_axis}
+import SkylineDominanceScoreUtils.SkylineDominanceScore2d_utils.create_grid_cells_to_check_2d
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{avg, col, max}
+import org.apache.spark.sql.types.DoubleType
+
+object Skyline_dominanceScore_2d {
+
+	def get_top_k_dominant_2d(k:Int, dataset_path:String, x_axis_size:Int, y_axis_size:Int): Unit ={
+
+		Logger.getLogger("org").setLevel(Level.WARN)
+		Logger.getLogger("akka").setLevel(Level.WARN)
+
+		val conf = new SparkConf()
+			.setAppName("SkylineDominanceScore2d_top" + k + "_" + dataset_path + "_" + x_axis_size + "_" + y_axis_size)
+		//			.set("spark.scheduler.mode", "FAIR")
+		val sparkSession = SparkSession.builder
+			.config(conf = conf)
+			.getOrCreate()
+
+		//correlated/correlated50000.csv
+		//uniform/uniform1000.csv
+		val df = sparkSession.read.option("header", "true").csv(dataset_path)
+			.select(col("0").cast(DoubleType).alias("x"), col("1").cast(DoubleType).alias("y"), col("id"))
+
+		val x_mean = df.select(avg("x")).first().getDouble(0)
+		val y_mean = df.select(avg("y")).first().getDouble(0)
+
+		val x_max = df.select(max("x")).first().getDouble(0)
+		val y_max = df.select(max("y")).first().getDouble(0)
+
+		val x_axis = create_grid_axis(x_mean, x_axis_size)
+		val y_axis = create_grid_axis(y_mean, y_axis_size)
+
+		// code to get skyline
+		//
+		//
+		//
+
+		val skyline = List((10.50646332,10.132206, 457),
+			(10.37956577, 11.12590036, 94500),
+			(13.12073157, 10.00053383, 5174),
+			(10.056831, 15.46942073, 9379),
+			(10.00818797, 22.65472621, 4884))
+
+
+		val grid_cells_to_check = create_grid_cells_to_check_2d(
+			df,
+			x_max,
+			y_max,
+			x_axis,
+			y_axis,
+			skyline,
+			k)
+
+		println("cells to check: " + grid_cells_to_check.size)
+		println("points to check: " + grid_cells_to_check.map(_._2._1).sum)
+
+		var points_checked = 0
+		import sparkSession.implicits._
+
+		var scoresDf = Seq.empty[(String, Int)].toDF("id", "score")
+
+		for(grid_cell <- grid_cells_to_check){
+
+			val x_line_left = grid_cell._2._2
+			val x_line_right = grid_cell._2._3
+			val y_line_up = grid_cell._2._4
+			val y_line_down = grid_cell._2._5
+
+			val cell_dominator_df = df.filter("x <= " + x_line_right + " AND y <= " + y_line_up +
+				" AND " + " x > " + x_line_left + " AND  y > " + y_line_down)
+
+			val cells_to_check_dominance_df = df.filter(
+				"( x > " + x_line_right + " AND y <= " + y_line_up + " AND y > " + y_line_down + " ) " +
+					" OR " + " ( y > " + y_line_up + " AND  x <= " + x_line_right +  " AND x > " + x_line_left + " ) " )
+
+			val guarantee_dominance_score = grid_cell._3._1
+
+			val cell_scores_df = calculate_dominance_score_2d(
+				cell_dominator_df,
+				cells_to_check_dominance_df.union(cell_dominator_df),
+				sparkSession,
+				guarantee_dominance_score)
+
+			scoresDf = scoresDf.union(cell_scores_df)
+
+		}
+
+		println(scoresDf.sort(col("score").desc).show(k))
+	}
+
+	def main(args: Array[String]): Unit = {
+
+		val k = args(0)
+		val dataset_path = args(1)
+		val x_axis_size = args(2)
+		val y_axis_size = args(3)
+		get_top_k_dominant_2d(k.toInt, dataset_path, x_axis_size.toInt, y_axis_size.toInt)
+	}
+}
